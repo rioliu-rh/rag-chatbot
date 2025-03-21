32b34cd490352cd4423047704bad5e821a08d2e7
Rio Liu <rioliu@redhat.com>
2025-03-21 15:57:13 +0800

    add logic to determine if retrieved docs are relevant to the user question

--- chatbot/llm/rag.py
+++ chatbot/llm/rag.py
@@ -1,7 +1,9 @@
 import warnings
 from langchain_community.llms import VLLMOpenAI
 from langchain import hub
-
+from langchain.chains import LLMChain
+from langchain.output_parsers import RegexParser
+from chatbot import llm
 from chatbot.config.config import ConfigHelper
 from chatbot.vectorstore.vectorstore import VectorStoreManager
 
@@ -13,7 +15,7 @@ class RetrievalAndGeneration():
         # disable warning log from langsmith
         warnings.filterwarnings("ignore")
         llm_conf = self.config.get_llm_config()
-        self.llm = llm = VLLMOpenAI(
+        self.llm = VLLMOpenAI(
             openai_api_key=llm_conf.api_key,
             openai_api_base=f"{llm_conf.api_url}/v1",
             model_name=llm_conf.model_name,
@@ -24,13 +26,29 @@ class RetrievalAndGeneration():
     def query(self, question):
         if not question:
             return "No user input found"
-        # define prompt for question-answering
-        # prompt template can be found from https://smith.langchain.com/hub/rlm/rag-prompt-mistral
-        # TODO: we can introduce a prompt selector/resolver to choose different template by model
-        prompt = hub.pull("rlm/rag-prompt-mistral", include_model=True)
+        
         retrieved_docs = self.vectore_store.similarity_search(question)
-        docs_content = "\n".join(doc.page_content for doc in retrieved_docs)
+        doc_contents = [doc.page_content for doc in retrieved_docs]
+        # check if retrieved docs are relevant to the user question
+        doc_relevance_prompt = hub.pull("rlm/rag-document-relevance")
+        output_parser = RegexParser(
+            regex=r"Answer: (\d)",
+            output_keys=["score"],
+            default_output_key="score"
+        )
+        llm_chain = LLMChain(prompt=doc_relevance_prompt, llm=self.llm, output_parser=output_parser)
+        result = llm_chain.invoke({"input": {"documents": doc_contents, "question": question}})
         if self.config.is_debug_logging_enabled():
-            print(f"queried docs:\n {docs_content}\n")
-        prompt = prompt.invoke({"question": question, "context": docs_content})
+            print(f"docs are relevant to user question: {result}")
+        prompt = None
+        if int(result['text']['score']) == 1:
+            # it means the retrieved docs are relevant to the user question, then call llm with rag prompt
+            # prompt template can be found from https://smith.langchain.com/hub/rlm/rag-prompt-mistral
+            # TODO: we can introduce a prompt selector/resolver to choose different template by model
+            prompt = hub.pull("rlm/rag-prompt-mistral", include_model=True)
+            docs_content = "\n".join(doc_contents)
+            prompt = prompt.invoke({"question": question, "context": docs_content})
+        else:
+            prompt = question
+            
         return self.llm.invoke(prompt)
